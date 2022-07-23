c086acb90346436d50b8a4a3b739e4c37f948384
Carolyn Van Slyck <me@carolynvanslyck.com>
2022-07-23 15:21:34 -0500

    Improve godoc for mgx package
    Signed-off-by: Carolyn Van Slyck <me@carolynvanslyck.com>

--- mgx/must.go
+++ mgx/must.go
@@ -2,7 +2,12 @@ package mgx
 
 import "github.com/magefile/mage/mg"
 
-// Must stops the build when an error occurs.
+// Must stops execution by throwing a panic when an error occurs.
+//
+// This may be used to keep your magefile brief, and mimic set -euo in an
+// equivalent bash script. This pattern works well in magefile targets only, not
+// helper functions. For helper functions, return an error so that you can write
+// tests and allow the calling function to handle the error.
 func Must(err error) {
 	if err != nil {
 		panic(mg.Fatal(1, err))
